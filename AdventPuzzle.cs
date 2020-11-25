d4bca69cba2c7c00e33375bfe1a772d4f21f44ec
mjazbc <mjazbec1@gmail.com>
2020-11-25 22:00:00 +0100

    fixed namespaces

--- AdventPuzzle.cs
+++ AdventPuzzle.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
-namespace Util
+namespace aoc_core
 {
     public abstract class AdventPuzzle
     {
--- AocInput.cs
+++ AocInput.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 
-namespace Util
+namespace aoc_core
 {
     public class AocInput
     {
