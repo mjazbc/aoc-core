0b517914acf71722a4563be547661244946509a9
MATIC JAZBEC <mj5726@student.uni-lj.si>
2020-12-02 16:53:25 +0100

    custom enumerable

--- AocInput.cs
+++ AocInput.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -31,7 +32,7 @@ namespace aoc_core
         public int[] AsIntArray(char separator = '\n') => ConvertToArray<int>(separator);
         public string[] AsStringArray(char separator = '\n') => ConvertToArray<string>(separator);
         public T AsCustomType<T>(Func<string, T> TypeConverter) => TypeConverter(_inputText);
-        public T[] AsCustomTypeArray<T>(Func<string, T[]> TypeConverter) => TypeConverter(_inputText);
+        public IEnumerable<T> AsCustomTypeEnumerable<T>(Func<string, T> TypeConverter, char separator = '\n') => _inputText.Split(separator).Select(l => TypeConverter(l));
         private T[] ConvertToArray<T>(char separator)
         {
             return _inputText.Split(separator).Select(l => (T)Convert.ChangeType(l, typeof(T))).ToArray();
