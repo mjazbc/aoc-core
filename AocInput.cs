191e4d46b0db858fa57a7bb19e8a4a42dd55b89b
MATIC JAZBEC <mj5726@student.uni-lj.si>
2020-12-05 13:25:18 +0100

    use environment line separator as default

--- AocInput.cs
+++ AocInput.cs
@@ -29,11 +29,15 @@ namespace aoc_core
         
         public int AsInt() => int.Parse(_inputText);
         public string AsString() => _inputText;
-        public int[] AsIntArray(string separator = "\r\n") => ConvertToArray<int>(separator);
-        public string[] AsStringArray(string separator = "\r\n") => ConvertToArray<string>(separator);
-        public char[][] AsCharMatrix(string separator = "\r\n") => AsStringArray(separator).Select(line => line.ToCharArray()).ToArray();
+        public int[] AsIntArray(string separator) => ConvertToArray<int>(separator);
+        public int[] AsIntArray() => ConvertToArray<int>(Environment.NewLine);
+        public string[] AsStringArray(string separator) => ConvertToArray<string>(separator);
+        public string[] AsStringArray() => ConvertToArray<string>(Environment.NewLine);
+        public char[][] AsCharMatrix(string separator) => AsStringArray(separator).Select(line => line.ToCharArray()).ToArray();
+        public char[][] AsCharMatrix() => AsStringArray(Environment.NewLine).Select(line => line.ToCharArray()).ToArray();
         public T AsCustomType<T>(Func<string, T> TypeConverter) => TypeConverter(_inputText);
-        public IEnumerable<T> AsCustomTypeEnumerable<T>(Func<string, T> TypeConverter, string separator = "\r\n") => _inputText.Split(separator).Select(l => TypeConverter(l));
+        public IEnumerable<T> AsCustomTypeEnumerable<T>(Func<string, T> TypeConverter, string separator) => _inputText.Split(separator).Select(l => TypeConverter(l));
+        public IEnumerable<T> AsCustomTypeEnumerable<T>(Func<string, T> TypeConverter) => _inputText.Split(Environment.NewLine).Select(l => TypeConverter(l));
         private T[] ConvertToArray<T>(string separator)
         {
             return _inputText.Split(separator).Select(l => (T)Convert.ChangeType(l, typeof(T))).ToArray();
